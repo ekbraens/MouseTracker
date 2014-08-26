a654ab4a3f06678651ff0008b08efcd022e7a316
New <eric.braen@gmail.com>
2014-08-25 23:33:22 -0700

    firster commit

---
+++ MouseTracker/MouseTracker/ViewViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewViewController.h
+//  MouseTracker
+//
+//  Created by New on 8/25/14.
+//  Copyright (c) 2014 New. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewViewController : UIViewController
+
+@end
---
+++ MouseTracker/MouseTracker/ViewViewController.m
@@ -0,0 +1,52 @@
+//
+//  ViewViewController.m
+//  MouseTracker
+//
+//  Created by New on 8/25/14.
+//  Copyright (c) 2014 New. All rights reserved.
+//
+
+#import "ViewViewController.h"
+
+@interface ViewViewController ()
+
+@end
+
+@implementation ViewViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor purpleColor];
+    UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"Hello!" message:@"Welcome to OpenCV" delegate:self cancelButtonTitle:@"Continue" otherButtonTitles:nil];
+    [alert show];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
